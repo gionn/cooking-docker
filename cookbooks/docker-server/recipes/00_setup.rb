5a493e8e3ffa4c5bd569bfdadf1a80123acccadd
Giovanni Toraldo <me@gionn.net>
2015-04-04 00:33:18 +0200

    Added basic recipes.
    01_nodejs requires fix in chef cookbook.

---
+++ cookbooks/docker-server/recipes/00_setup.rb
@@ -0,0 +1,11 @@
+#
+# Cookbook Name:: docker-server
+# Recipe:: default
+#
+# Copyright 2015, YOUR_COMPANY_NAME
+#
+# All rights reserved - Do Not Redistribute
+
+include_recipe 'docker'
+
+docker_image 'ubuntu:14.04'
